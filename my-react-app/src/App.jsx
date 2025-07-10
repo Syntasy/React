accb63d022f6c6b3246cc0868809d97e412b8386
Syntasy <schullarj@proton.me>
2025-07-10 17:12:08 -0400

    working on cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,11 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from './Card.jsx'
 
 function App() {
+    return(
+        <Card />
+    );
 
-  return(
-    <>
-    <Header/>
-    <Food/>
-    <Footer/>
-    </>
-
-    
-  );
+ 
   
 }
 
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="/src/assets/Profilepic.jpg" alt="pokemon"></img>
+            <h2>James</h2>
+            <p>I cook food, learning react.</p>
+        </div>
+    );
+
+}
+
+export default Card
